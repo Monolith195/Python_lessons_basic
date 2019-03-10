60d37c1f74551f1bea242e9d4d0171a7d0a0127a
Monolith195 <monolith195@gmail.com>
2019-03-10 20:44:36 +0300

    Lesson5 (easy, normal)

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -1,10 +1,41 @@
+import os
+import shutil
+
+
 # Задача-1:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+def make():
+    for i in range(1, 10):
+        try:
+            os.mkdir(os.path.join(os.getcwd(), 'dir_{}'.format(i)))
+        except FileExistsError as e:
+            print('{} - уже существует'.format(e.filename))
+
+
+def remove():
+    for i in range(1, 10):
+        try:
+            os.rmdir(os.path.join(os.getcwd(), 'dir_{}'.format(i)))
+        except FileNotFoundError as e:
+            print('{} - не существует'.format(e.filename))
+
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
+def show_files():
+    files = os.listdir(path=os.getcwd())
+    if not files:
+        print("Папка пуста\n")
+    else:
+        print('Файлы и папки:\n', *files, sep='\n')
+
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+def copy_current_file():
+    cp = os.path.basename(__file__).split(".")
+    shutil.copy(os.path.basename(__file__), "{}-copy.{}".format(cp[0], cp[1]))
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -13,3 +13,67 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+import os
+from lesson05.home_work.hw05_easy import show_files
+
+
+def remove():
+    rm = input("Введите название папки: ")
+    try:
+        os.rmdir(os.path.join(os.getcwd(), rm))
+        print('Папка {} успешно удалена'.format(rm))
+    except FileNotFoundError as e:
+        print('{} - невозможно удалить. Папки не существует'.format(e.filename))
+    except OSError as o:
+        print('{} - невозможно удалить. Папка не пустая'.format(o.filename))
+
+
+def make():
+    mk = input("Введите название папки: ")
+    try:
+        os.mkdir(os.path.join(os.getcwd(), mk))
+        print('Папка {} успешно создана'.format(mk))
+    except FileExistsError as e:
+        print('Папка {} - уже существует'.format(e.filename))
+
+
+def change_dir():
+    cd = input("Введите название папки: ")
+    try:
+        os.chdir(cd)
+        print("Перешел в папку: {}".format(cd))
+    except OSError as e:
+        print("Папка {} не найдена".format(e.filename))
+
+
+def start():
+    while True:
+        try:
+            user_input = int(input("\nВыбор действия:\n"
+                                   "1. Перейти в папку\n"
+                                   "2. Просмотреть содержимое текущей папки\n"
+                                   "3. Удалить папку\n"
+                                   "4. Создать папку\n"
+                                   "0. Выход\n"
+                                   "==============================\n"))
+            if user_input == 1:
+                change_dir()
+
+            if user_input == 2:
+                show_files()
+
+            if user_input == 3:
+                remove()
+
+            if user_input == 4:
+                make()
+
+            if user_input == 0:
+                break
+
+        except ValueError:
+            print("Такого действия не существует")
+
+
+start()
