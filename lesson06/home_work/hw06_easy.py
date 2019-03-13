84220880e17e77deccb529e992519229b1a979ec
Monolith195 <monolith195@gmail.com>
2019-03-13 20:09:37 +0300

    Lesson6 (easy, normal)

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,9 +1,43 @@
-# Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
-# Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
+# Задача - 1
+# Опишите несколько классов TownCar, SportCar, WorkCar, PoliceCar
+# У каждого класса должны быть следующие аттрибуты:
+# speed, color, name, is_police - Булево значение.
+# А так же несколько методов: go, stop, turn(direction) - которые должны сообщать,
+#  о том что машина поехала, остановилась, повернула(куда)
+
+# Задача - 2
+# Посмотрите на задачу-1 подумайте как выделить общие признаки классов
+# в родительский и остальные просто наследовать от него.
 
 
-# Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
-# Предусмотреть в классе методы:
-# проверка, является ли фигура равнобочной трапецией;
-# вычисления: длины сторон, периметр, площадь.
+class Car:
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed;
+        self.color = color;
+        self.name = name;
+        self.is_police = is_police;
 
+    def go(self):
+        print("{} поехала".format(self.name))
+
+    def stop(self):
+        print("{} остановилась".format(self.name))
+
+    def turn(self, direction):
+        print("{} повернула {}".format(self.name, direction))
+
+
+class TownCar(Car):
+    pass
+
+
+class SportCar(Car):
+    pass
+
+
+class WorkCar(Car):
+    pass
+
+
+class PoliceCar(Car):
+    pass
