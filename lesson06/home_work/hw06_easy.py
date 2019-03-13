3e03605fad3c936f93e3e23e3eb6f85467eb30c9
Monolith195 <monolith195@gmail.com>
2019-03-13 20:33:11 +0300

    Lesson6 (remove semicolons)

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -12,10 +12,10 @@
 
 class Car:
     def __init__(self, speed, color, name, is_police):
-        self.speed = speed;
-        self.color = color;
-        self.name = name;
-        self.is_police = is_police;
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
 
     def go(self):
         print("{} поехала".format(self.name))
